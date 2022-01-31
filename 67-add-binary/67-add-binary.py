9255c56ce12ec3ef794893e859a5cc1c31b9094d
aj-1904 <jainayush827@gmail.com>
2022-01-31 18:08:41 +0530

    Time: 65 ms (12.71%), Space: 13.9 MB (99.48%) - LeetHub

---
+++ 67-add-binary/67-add-binary.py
@@ -0,0 +1,21 @@
+class Solution:
+    def addBinary(self, a: str, b: str) -> str:
+        carry = 0
+        c1 = len(a)-1
+        c2 = len(b)-1
+        result = ''
+        while c1 >= 0 or c2 >= 0:
+            sum = carry
+            if c1 >= 0:
+                sum += ord(a[c1]) - ord('0')
+            if c2 >= 0:
+                sum += ord(b[c2]) - ord('0')
+            c1 -= 1
+            c2 -= 1
+            carry = 1 if sum > 1 else 0
+            result += str(sum % 2)
+        if carry:
+            result += str(carry)
+        return result[::-1]
+
+        
