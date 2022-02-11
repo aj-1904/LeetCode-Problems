97a6a8b80bdf3eb01f23d4572d0efc76bf27800d
aj-1904 <jainayush827@gmail.com>
2022-02-11 18:34:24 +0530

    Update 560-subarray-sum-equals-k.py

--- 560-subarray-sum-equals-k/560-subarray-sum-equals-k.py
+++ 560-subarray-sum-equals-k/560-subarray-sum-equals-k.py
@@ -2,7 +2,7 @@ class Solution:
     def subarraySum(self, nums: List[int], k: int) -> int:
         '''1. Use a dictionary to store the sum
             2. Check if sum is == k then increment the count
-            3. Check if sum-k is present in dictionary then increment the count by the                      frequency present in hash_table'''
+            3. Check if sum-k is present in dictionary then increment the count by the frequency present in hash_table'''
         hash_table = dict()
         sum = 0
         count = 0
