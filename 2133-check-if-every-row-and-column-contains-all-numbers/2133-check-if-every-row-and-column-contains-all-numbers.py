6a810eb3b46e032e358b23a32587338c84cc8f9b
aj-1904 <jainayush827@gmail.com>
2022-02-16 14:58:11 +0530

    Time: 1117 ms (46.06%), Space: 14.4 MB (98.44%) - LeetHub

---
+++ 2133-check-if-every-row-and-column-contains-all-numbers/2133-check-if-every-row-and-column-contains-all-numbers.py
@@ -0,0 +1,20 @@
+class Solution:
+    def checkValid(self, matrix: List[List[int]]) -> bool:
+        n = len(matrix)
+        
+        if n == 1:
+            return True
+    
+        row_set = set()
+        for i in range(n):
+            row_set = set()
+            col_set = set()
+            for j in range(n):
+                row_set.add(matrix[i][j])
+                col_set.add(matrix[j][i])
+                
+            if len(row_set) != n or len(col_set) != n:
+                return False
+        return True
+            
+        
