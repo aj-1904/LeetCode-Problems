919c14c06fc9dd8537be8fdfa92c864945fd8f74
Ayush Jain <jainayush827@gmail.com>
2022-06-26 17:51:00 +0530

    Time: 900 ms (6.15%), Space: 27.2 MB (71.96%) - LeetHub

---
+++ 1423-maximum-points-you-can-obtain-from-cards/1423-maximum-points-you-can-obtain-from-cards.py
@@ -0,0 +1,30 @@
+class Solution:
+    def maxScore(self, cardPoints: List[int], k: int) -> int:
+#         This problem is similar to minimum sum subarray of size k. Here we require maximum score.
+        '''This makes the thing clear that if we have to pick k cards either from the beginning or end such that their sum is maximum, we have to select a subarray of length size-k whose sum is minimum. So we will find that sum and remove that from the total sum of array elements.'''
+        n = len(cardPoints)
+        i = 0
+        j = 0
+        tsum = sum(cardPoints)
+        minSum = float('inf')
+        currSum = 0
+        
+        if n == k:
+            return tsum
+        
+        while j < n:
+            currSum += cardPoints[j]
+            
+            # if j-i+1 < k:
+            #     j += 1
+            if j-i+1 == n-k:
+                minSum = min(minSum, currSum)
+               # move the window
+                currSum = currSum - cardPoints[i]
+                i += 1
+            j += 1
+        return tsum-minSum
+            
+            
+            
+                    
