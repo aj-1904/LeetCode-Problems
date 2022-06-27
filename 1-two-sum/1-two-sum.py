eaee95ed47b3696728304131cc1ad20360ab457d
Ayush Jain <jainayush827@gmail.com>
2022-06-28 01:30:00 +0530

    Time: 83 ms (73.05%), Space: 15.3 MB (24.16%) - LeetHub

--- 1-two-sum/1-two-sum.py
+++ 1-two-sum/1-two-sum.py
@@ -1,21 +1,12 @@
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
-        # Brute Force - O(n^2)
-        # for i in range(0,len(nums)):
-        #     for j in range(i+1,len(nums)):
-        #         if nums[i] + nums[j] == target:
-        #             return [i,j]
+        n = len(nums)
+        hmap = dict()
         
-        # Optimized - Using hashmap - O(n)
-        
-        dictionary = dict()
-        for i in range(len(nums)):
-            if target - nums[i] in dictionary:
-                return [dictionary[target-nums[i]],i]
+        for i in range(n):
+            if target-nums[i] in hmap:
+                return [hmap[target-nums[i]], i]
             else:
-                dictionary[nums[i]] = i
-                
+                hmap[nums[i]] = i
             
-        
-        
         
