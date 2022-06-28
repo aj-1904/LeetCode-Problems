bcf91cd3f5a2ef3814230ea7482b7cc9a915273d
Ayush Jain <jainayush827@gmail.com>
2022-06-28 13:20:53 +0530

    Time: 372 ms (26.64%), Space: 14.8 MB (51.90%) - LeetHub

--- 1647-minimum-deletions-to-make-character-frequencies-unique/1647-minimum-deletions-to-make-character-frequencies-unique.py
+++ 1647-minimum-deletions-to-make-character-frequencies-unique/1647-minimum-deletions-to-make-character-frequencies-unique.py
@@ -5,12 +5,14 @@ class Solution:
         uniquelst = []
         count = 0
         
+        '''store the frequency of each character in dictionary'''
         for i in range(len(s)):
             if s[i] in countMap:
                 countMap[s[i]] += 1
             else:
                 countMap[s[i]] = 1
-                
+        
+        '''If similar value already present then decrement  '''
         for value in countMap.values():
             while value > 0 and value in uniquelst:
                 count += 1
