76ba0c197b3b0b196fb89b3c94b283e74fb85078
Ayush Jain <jainayush827@gmail.com>
2022-07-01 12:59:51 +0530

    Time: 165 ms (91.00%), Space: 14.3 MB (83.07%) - LeetHub

--- 1710-maximum-units-on-a-truck/1710-maximum-units-on-a-truck.py
+++ 1710-maximum-units-on-a-truck/1710-maximum-units-on-a-truck.py
@@ -1,6 +1,11 @@
 class Solution:
     def maximumUnits(self, boxTypes: List[List[int]], truckSize: int) -> int:
         
+        '''Greedy approach
+            TC - O(nlogn)
+            SC - O(1)'''
+        
+        # Sort boxes so that boxes with the most units appear first        
         boxTypes.sort(key=lambda x: x[1], reverse=True)
         maxUnits = 0
         
